0db74fa21c9f9a247922a57f51d7f515380e33fd
Thaddeus Ternes <tternes@gmail.com>
2011-05-24 02:18:07 -0500

    Remove nslog call

--- GSTwitPicEngine.m
+++ GSTwitPicEngine.m
@@ -92,9 +92,6 @@
     NSURL *url = [NSURL URLWithString:urlString];
     
     OAConsumer *consumer = [[[OAConsumer alloc] initWithKey:TWITTER_OAUTH_CONSUMER_KEY secret:TWITTER_OAUTH_CONSUMER_SECRET] autorelease];
-    
-    // NSLog(@"consumer: %@", consumer);
-    
     OARequestHeader *requestHeader = [[[OARequestHeader alloc] initWithProvider:@"https://api.twitter.com/1/account/verify_credentials.json"
                                                                          method:@"GET"
                                                                        consumer:consumer
